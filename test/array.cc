a7cbf5d13a887aa2bcbf41d8e3bfc8891b7327b5
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-11-22 21:42:28 +1100

    Add test

--- test/array.cc
+++ test/array.cc
@@ -4,9 +4,20 @@
 #include "arena.c"
 #include "array.c"
 
-Arena* arena = newArena(MEGABYTES(16));
-
 TEST(Array, newArray) {
+    Arena* arena = newArena(MEGABYTES(16));
+
     Array_i32* a = newArray_i32(arena);
     EXPECT_EQ(16, a->capacity);
+
+    freeArena(arena);
+}
+
+TEST(Array, newArrayc) {
+    Arena* arena = newArena(MEGABYTES(16));
+
+    Array_i32* a = newArray_i32c(arena, 32);
+    EXPECT_EQ(32, a->capacity);
+
+    freeArena(arena);
 }
