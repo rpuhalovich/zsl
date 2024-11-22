b8bfd6cfd863491efae7681eeea331f05f6f36cb
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-11-23 09:35:40 +1100

    Update comments

--- src/arena.h
+++ src/arena.h
@@ -9,8 +9,8 @@
 #define TERABYTES(number) (GIGABYTES(number) * 1024ull)
 
 // clang-format off
-// macos memory management: // <https://developer.apple.com/library/archive/documentation/Performance/Conceptual/ManagingMemory/Articles/AboutMemory.html>
-// Untangling Lifetimes - The Arena Allocator: // <https://www.rfleury.com/p/untangling-lifetimes-the-arena-allocator>
+// macos memory management: https://developer.apple.com/library/archive/documentation/Performance/Conceptual/ManagingMemory/Articles/AboutMemory.html
+// Untangling Lifetimes - The Arena Allocator: https://www.rfleury.com/p/untangling-lifetimes-the-arena-allocator
 // clang-format on
 
 typedef struct {
