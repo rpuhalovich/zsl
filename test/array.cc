0901e43d898e854f0625a57ae6de7c16eee72534
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-11-24 20:58:41 +1100

    Simplify array names

--- test/array.cc
+++ test/array.cc
@@ -6,47 +6,47 @@ extern "C" {
 #include "types.h"
 }
 
-TEST(Array, COMPOSITE_aPop_aGet_success) {
+TEST(Array, COMPOSITE_pop_getr_success) {
     Arena* arena = newArena(MEGABYTES(16));
 
     Array_i32* a = newArrayc_i32(arena, 1);
     EXPECT_EQ(1, a->capacity);
 
-    aAppend_i32(arena, a, 1);
-    aAppend_i32(arena, a, 2);
-    aAppend_i32(arena, a, 3);
+    push_i32(arena, a, 1);
+    push_i32(arena, a, 2);
+    push_i32(arena, a, 3);
     EXPECT_EQ(3, a->length);
     EXPECT_EQ(4, a->capacity);
 
-    i32 i = aGet_i32(a, a->length - 1);
+    i32 i = get_i32(a, a->length - 1);
     EXPECT_EQ(3, i);
 
-    aPop_i32(a);
-    i = aGet_i32(a, a->length - 1);
+    pop_i32(a);
+    i = get_i32(a, a->length - 1);
     EXPECT_EQ(2, i);
 
     freeArena(arena);
 }
 
-TEST(Array, COMPOSITE_aGet_aAppend_aGet_aPop_aGet_success) {
+TEST(Array, COMPOSITE_get_push_get_pop_get_success) {
     Arena* arena = newArena(MEGABYTES(16));
 
     Array_i32* a = newArrayc_i32(arena, 1);
     EXPECT_EQ(1, a->capacity);
 
-    aAppend_i32(arena, a, 1);
-    aAppend_i32(arena, a, 2);
-    aAppend_i32(arena, a, 3);
-    aAppend_i32(arena, a, 4);
-    aAppend_i32(arena, a, 5);
+    push_i32(arena, a, 1);
+    push_i32(arena, a, 2);
+    push_i32(arena, a, 3);
+    push_i32(arena, a, 4);
+    push_i32(arena, a, 5);
     EXPECT_EQ(5, a->length);
     EXPECT_EQ(8, a->capacity);
 
-    i32 i = aGet_i32(a, a->length - 1);
+    i32 i = get_i32(a, a->length - 1);
     EXPECT_EQ(5, i);
 
-    aPop_i32(a);
-    i = aGet_i32(a, a->length - 1);
+    pop_i32(a);
+    i = get_i32(a, a->length - 1);
     EXPECT_EQ(4, a->length);
     EXPECT_EQ(8, a->capacity);
     EXPECT_EQ(4, i);
@@ -54,34 +54,34 @@ TEST(Array, COMPOSITE_aGet_aAppend_aGet_aPop_aGet_success) {
     freeArena(arena);
 }
 
-TEST(Array, aGet_out_of_bounds_fail) {
+TEST(Array, get_out_of_bounds_fail) {
     Arena* arena = newArena(MEGABYTES(16));
 
     Array_i32* a = newArrayc_i32(arena, 1);
-    aAppend_i32(arena, a, 1);
-    aAppend_i32(arena, a, 2);
-    aAppend_i32(arena, a, 3);
-    aAppend_i32(arena, a, 4);
-    aAppend_i32(arena, a, 5);
+    push_i32(arena, a, 1);
+    push_i32(arena, a, 2);
+    push_i32(arena, a, 3);
+    push_i32(arena, a, 4);
+    push_i32(arena, a, 5);
     EXPECT_EQ(5, a->length);
     EXPECT_EQ(8, a->capacity);
 
-    Result_i32 i = aGetr_i32(a, a->length);
+    Result_i32 i = getr_i32(a, a->length);
     EXPECT_EQ(true, i.fail);
     EXPECT_EQ(0, i.result);
     EXPECT_EQ(ERROR_OUT_OF_BOUNDS, i.error);
 
-    i = aGetr_i32(a, INT_MAX);
+    i = getr_i32(a, INT_MAX);
     EXPECT_EQ(true, i.fail);
     EXPECT_EQ(0, i.result);
     EXPECT_EQ(ERROR_OUT_OF_BOUNDS, i.error);
 
-    i = aGetr_i32(a, INT_MIN);
+    i = getr_i32(a, INT_MIN);
     EXPECT_EQ(true, i.fail);
     EXPECT_EQ(0, i.result);
     EXPECT_EQ(ERROR_OUT_OF_BOUNDS, i.error);
 
-    i = aGetr_i32(a, INT_MIN + 1);
+    i = getr_i32(a, INT_MIN + 1);
     EXPECT_EQ(true, i.fail);
     EXPECT_EQ(0, i.result);
     EXPECT_EQ(ERROR_OUT_OF_BOUNDS, i.error);
