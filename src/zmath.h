dcd94d1b84aa598bf67e27987430b521be539a7d
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-12-10 20:59:03 +1100

    Rename math types

--- src/zmath.h
+++ src/zmath.h
@@ -6,18 +6,18 @@
 typedef struct {
     f32 x;
     f32 y;
-} Vector2f;
+} v2f;
 
 typedef struct {
     i32 x;
     i32 y;
-} Vector2i;
+} v2i;
 
 typedef struct {
     f32 x;
     f32 y;
     f32 width;
     f32 height;
-} ZRectangle;
+} rec;
 
 #endif // ZMATH_H
